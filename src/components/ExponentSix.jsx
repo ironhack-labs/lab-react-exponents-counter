2917278ce714c604b0b42c3a1f4c9583bb59f7fd
Janos Sitku <sitku.janos86@gmail.com>
2023-12-14 12:24:42 +0100

    first

--- src/components/ExponentSix.jsx
+++ src/components/ExponentSix.jsx
@@ -1,6 +1,6 @@
-const ExponentSix = () => (
+const ExponentSix = ({count}) => (
   <div className="exponent-counter-container">
-    <p className="exponent-label">n⁶</p>
+    <p className="exponent-label">{count}⁶</p>
     <p className="exponent-result">2 * 2 * 2 * 2 * 2 * 2 = <span className="total">64</span></p>
   </div>
 );
