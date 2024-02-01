843b365e13b443548ad926a20db87b0e6d0c6c85
Olumide <wenceslausajidahu@gmail.com>
2024-02-01 15:28:55 +0100

    Olumide-react-exponents

--- src/components/ExponentThree.jsx
+++ src/components/ExponentThree.jsx
@@ -1,7 +1,10 @@
-const ExponentThree = () => (
-   <div className="exponent-counter-container">
-    <p className="exponent-label">n³</p>
-    <p className="exponent-result">2 * 2 * 2 = <span className="total">8</span></p>
+const ExponentThree = ({ counter }) => (
+  <div className="exponent-counter-container">
+    <p className="exponent-label">{counter}³</p>
+    <p className="exponent-result">
+      {counter} *{counter} *{counter} ={" "}
+      <span className="total">{Math.pow(counter, 3)}</span>
+    </p>
   </div>
 );
 
