722fd4726a1cc2ce660a0017e0b57aa629a2eb1a
TinyjoyTW <152999162+TinyjoyTW@users.noreply.github.com>
2024-02-01 15:07:24 +0100

    solve exponent counter lab

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,32 @@ import ExponentThree from "./components/ExponentThree";
 import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
+import { useState } from "react";
 
+function App() {
+  const [counter, setCounter] = useState(0);
+  const decrement = () => setCounter((prevCount) => prevCount - 1);
+  const increment = () => setCounter((prevCount) => prevCount + 1);
 
-function App () {
   return (
     <div className="App">
-      <h2><em>Counter</em></h2>
-  
-      <Counter/>
+      <h2>
+        <em>Counter</em>
+      </h2>
+
+      <Counter counter={counter} increment={increment} decrement={decrement} />
 
       <br />
-      <h2><em>Exponents</em></h2>
+      <h2>
+        <em>Exponents</em>
+      </h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+        <ExponentTwo counter={counter} />
+        <ExponentThree counter={counter} />
+        <ExponentFour counter={counter} />
+        <ExponentFive counter={counter} />
+        <ExponentSix counter={counter} />
       </div>
     </div>
   );
