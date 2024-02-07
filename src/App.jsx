e7fb82d0c4c87ca41b29f362ab68de63655a6df2
chyVacheck <dmytro.shah.work@gmail.com>
2024-02-07 10:55:54 +0100

    done + feat
    - add universal Exponent
    - render 20 exponent instead 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,53 @@
+import { useState } from "react";
+
 import "./App.css";
 import Counter from "./components/Counter";
-import ExponentTwo from "./components/ExponentTwo";
-import ExponentThree from "./components/ExponentThree";
-import ExponentFour from "./components/ExponentFour";
-import ExponentFive from "./components/ExponentFive";
-import ExponentSix from "./components/ExponentSix";
+import Exponent from "./components/Exponent";
+// import ExponentTwo from "./components/ExponentTwo";
+// import ExponentThree from "./components/ExponentThree";
+// import ExponentFour from "./components/ExponentFour";
+// import ExponentFive from "./components/ExponentFive";
+// import ExponentSix from "./components/ExponentSix";
 
+function App() {
+  const [count, setCount] = useState(0);
 
-function App () {
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+  const increment = () => setCount((prevCount) => prevCount + 1);
+
   return (
     <div className="App">
-      <h2><em>Counter</em></h2>
-  
-      <Counter/>
+      <h2>
+        <em>Counter</em>
+      </h2>
+
+      <Counter count={count} decrement={decrement} increment={increment} />
 
       <br />
-      <h2><em>Exponents</em></h2>
+      <h2>
+        <em>Exponents</em>
+      </h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+        <Exponent count={count} exp={2} />
+        <Exponent count={count} exp={3} />
+        <Exponent count={count} exp={4} />
+        <Exponent count={count} exp={5} />
+        <Exponent count={count} exp={6} />
+        <Exponent count={count} exp={7} />
+        <Exponent count={count} exp={8} />
+        <Exponent count={count} exp={9} />
+        <Exponent count={count} exp={10} />
+        <Exponent count={count} exp={11} />
+        <Exponent count={count} exp={12} />
+        <Exponent count={count} exp={13} />
+        <Exponent count={count} exp={14} />
+        <Exponent count={count} exp={15} />
+        <Exponent count={count} exp={16} />
+        <Exponent count={count} exp={17} />
+        <Exponent count={count} exp={18} />
+        <Exponent count={count} exp={19} />
+        <Exponent count={count} exp={20} />
       </div>
     </div>
   );
