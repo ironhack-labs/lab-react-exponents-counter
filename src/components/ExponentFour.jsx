2712071de5432ffe5fb4fbe051e205090b7bf280
Marianna Di Vito <maridivi89@gmail.com>
2024-02-07 16:25:14 +0000

    done

--- src/components/ExponentFour.jsx
+++ src/components/ExponentFour.jsx
@@ -1,7 +1,9 @@
 const ExponentFour = () => (
-   <div className="exponent-counter-container">
+  <div className="exponent-counter-container">
     <p className="exponent-label">n⁴</p>
-    <p className="exponent-result">2 * 2 * 2 * 2 = <span className="total">16</span></p>
+    <p className="exponent-result">
+      2 * 2 * 2 * 2 = <span className="total">16</span>
+    </p>
   </div>
 );
 
