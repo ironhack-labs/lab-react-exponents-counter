d400fd14f9272b734c73f5c1123e3541648621fb
Huguiz <hugo.maurin@hotmail.com>
2024-02-08 12:45:59 +0100

    Lab finished

--- src/components/ExponentFour.jsx
+++ src/components/ExponentFour.jsx
@@ -1,7 +1,7 @@
-const ExponentFour = () => (
+const ExponentFour = (props) => (
    <div className="exponent-counter-container">
     <p className="exponent-label">n⁴</p>
-    <p className="exponent-result">2 * 2 * 2 * 2 = <span className="total">16</span></p>
+    <p className="exponent-result">{props.counter} * {props.counter} * {props.counter} * {props.counter} = <span className="total">{props.counter**4}</span></p>
   </div>
 );
 
