9eba8ec51188ba958faaadbcfce695457c134750
Büsra Kocer <busustaoglu@gmail.com>
2024-02-08 20:19:02 +0100

    solved

--- src/components/ExponentThree.jsx
+++ src/components/ExponentThree.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
 
-const ExponentThree = (props) => (
-   <div className="exponent-counter-container">
-    <p className="exponent-label">{props.count}³</p>
-    <p className="exponent-result">2 * 2 * 2 = <span className="total">8</span></p>
-  </div>
-);
+const ExponentThree = (props) => { 
+
+  return (
+    <div className="exponent-counter-container">
+      <p className="exponent-label">{props.count}³ </p>
+      <p className="exponent-result">
+        {props.count} * {props.count} * {props.count}  ={" "}
+        <span className="total">{props.calculateExponent(props.count, 3)}</span>
+      </p>
+    </div>
+  )
+  
+    };
 
 export default ExponentThree;
--- src/components/ExponentTwo.jsx
+++ src/components/ExponentTwo.jsx
@@ -1,12 +1,19 @@
 
 import React from "react";
 
-const ExponentTwo = (props) => (
+const ExponentTwo = (props) => { 
 
-   <div className="exponent-counter-container">
-    <p className="exponent-label">{props.count}²</p>
-    <p className="exponent-result">{props.count} * {props.count} = <span className="total">4</span></p>
-  </div>
-);
+  return (
+    <div className="exponent-counter-container">
+      <p className="exponent-label">{props.count}² </p>
+      <p className="exponent-result">
+        {props.count} * {props.count}  ={" "}
+        <span className="total">{props.calculateExponent(props.count, 2)}</span>
+      </p>
+    </div>
+  )
+  
+    };
+  
 
 export default ExponentTwo;
