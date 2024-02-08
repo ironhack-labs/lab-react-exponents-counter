b2245b4f60c135d1eab8584ffe54bb89023aafe5
Stefan.Kummerlöw <gummiz@gmx.net>
2024-02-08 15:39:50 +0100

    update on ExponentSix as i use 4 instead of 6

--- src/components/ExponentSix.jsx
+++ src/components/ExponentSix.jsx
@@ -1,7 +1,7 @@
 const ExponentSix = ({count}) => (
   <div className="exponent-counter-container">
     <p className="exponent-label">{count}⁶</p>
-    <p className="exponent-result">{count} * {count} * {count} * {count} * {count} * {count} = <span className="total">{count ** 4}</span></p>
+    <p className="exponent-result">{count} * {count} * {count} * {count} * {count} * {count} = <span className="total">{count ** 6}</span></p>
   </div>
 );
 
