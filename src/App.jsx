f1ce09c363578e16fb9961a7539f15a935a38216
Stefan.Kummerlöw <gummiz@gmx.net>
2024-02-08 14:43:42 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,44 @@ import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
 import { useState } from "react";
+import Exponent from "./components/Exponent";
 
-
-function App () {
+function App() {
   const [count, setCount] = useState(0);
   const decrement = () => setCount((prevCount) => prevCount - 1);
   const increment = () => setCount((prevCount) => prevCount + 1);
 
   return (
     <div className="App">
-      <h2><em>Counter</em></h2>
-  
-      <Counter count={count} decrement={decrement} increment={increment}/>
+      <h2>
+        <em>Counter</em>
+      </h2>
+
+      <Counter count={count} decrement={decrement} increment={increment} />
 
       <br />
-      <h2><em>Exponents</em></h2>
+      <h2>
+        <em>Exponents</em>
+      </h2>
 
       <div className="container">
-        <ExponentTwo count={count}/>
-        <ExponentThree count={count}/>
-        <ExponentFour count={count}/>
-        <ExponentFive count={count}/>
-        <ExponentSix count={count}/>
+        <ExponentTwo count={count} />
+        <ExponentThree count={count} />
+        <ExponentFour count={count} />
+        <ExponentFive count={count} />
+        <ExponentSix count={count} />
+      </div>
+
+      <h2>
+        <em>Bonus Exponents</em>
+      </h2>
+      <div className="container">
+        <Exponent count={count} power={1} />
+        <Exponent count={count} power={2} />
+        <Exponent count={count} power={3} />
+        <Exponent count={count} power={4} />
+        <Exponent count={count} power={5} />
+        <Exponent count={count} power={6} />
       </div>
     </div>
   );
