1aed465d01bc38eed901d82c302044131cd8eb99
Stefan.Kummerlöw <gummiz@gmx.net>
2024-02-08 14:20:19 +0100

    done without Bonus

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
+const Counter = ({count, increment, decrement}) => {
 
-  const decrement = () => setCount((prevCount) => prevCount - 1);
-  const increment = () => setCount((prevCount) => prevCount + 1);
+
 
   return (
     <div className="counter-container">
