80a6f1d1b67c6577d233f0963bad6de2e6428c96
Abranucu <abrahamcastiellobarbas@gmail.com>
2024-02-16 23:28:55 +0100

    falta bonus

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
-
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const decrement = () => setCount((prevCount) => prevCount - 1);
-  const increment = () => setCount((prevCount) => prevCount + 1);
+const Counter = (props) => {
+  let count = props.count;
+  const decrement = () => props.setCount((count) => count - 1);
+  const increment = () => props.setCount((count) => count + 1);
 
   return (
     <div className="counter-container">
       <p className="counter-value">{count}</p>
-      <button className="counter-button" onClick={decrement}>-</button>
-      <button className="counter-button" onClick={increment}>+</button>
+      <button className="counter-button" onClick={decrement}>
+        -
+      </button>
+      <button className="counter-button" onClick={increment}>
+        +
+      </button>
     </div>
   );
 };
--- src/components/ExponentFour.jsx
+++ src/components/ExponentFour.jsx
@@ -1,8 +1,14 @@
-const ExponentFour = () => (
-   <div className="exponent-counter-container">
-    <p className="exponent-label">n⁴</p>
-    <p className="exponent-result">2 * 2 * 2 * 2 = <span className="total">16</span></p>
-  </div>
-);
+const ExponentFour = (props) => {
+  let count = props.count;
+  let result = count * count * count * count;
+  return (
+    <div className="exponent-counter-container">
+      <p className="exponent-label">{count}⁴</p>
+      <p className="exponent-result">
+        {count} * {count} * {count} * {count} = <span className="total">{result}</span>
+      </p>
+    </div>
+  );
+};
 
 export default ExponentFour;
